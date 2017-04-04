3493f5d29ef0012f81ac868a0a34c7a78dc87009
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-04-04 14:20:40 -0400

    Added Bootstrap Image

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Menu from './menu';
+import {Image} from 'react-bootstrap';
 
-/*Render Home Component and apply it root id in index.html */
- // ReactDOM.render(
- //    <Home/>,
- //      document.querySelector('.root')
- // );
 export default class App extends Component {
   render() {
     return (
       <div>
         <Menu/>
       <div id="map">
-        <img src="static/img/C_Camo_Map.png"/>
+        <Image src="static/img/C_Camo_Map.png" responsive/>
       </div>
         {this.props.children}
       </div>
