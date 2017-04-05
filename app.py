96130724aaf95df21905559b838dfcb80232deba
bearreport <bayerj@gmail.com>
2017-04-04 22:57:24 -0400

    restful api alpha

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #import flask, flask extensions and flask functions
-from flask import Flask, request, redirect, jsonify, render_template, flash, url_for
+from flask import Flask, request, redirect, jsonify, render_template, flash, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 #import helpers
 import json
+import datetime
 #import local functions and variables
-from userpins import addPin, approvePin, denyPin
+from userpins import addPin, approvePin, denyPin, pinsToJsonDb, object_as_dict
 #import secret keys and such
 from config import *
 
@@ -13,14 +14,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://academypgh:breakfastatshellys@warringtons.crqrglgmlxa2.us-east-1.rds.amazonaws.com/warringtons'
 db=SQLAlchemy(app)
 
-class UserPin(db.Model):
-    __tablename__='example'
-    id = db.Column('id', db.INT, primary_key=True)
-    data = db.Column('data', db.VARCHAR(100))
 
-    def __init__(self, id, data):
+class UserPin(db.Model):
+    __tablename__='contycamo2'
+    id = db.Column('id', db.INT, primary_key=True)
+    lat = db.Column('lat', db.VARCHAR(45))
+    lng = db.Column('lng', db.VARCHAR(45))
+    zipcode = db.Column('zipcode', db.VARCHAR(45))
+    story = db.Column('story', db.VARCHAR(255))
+    diagnosis = db.Column('diagnosis', db.VARCHAR(45))
+    datetime = db.Column('datetime', db.VARCHAR(45))
+
+    def __init__(self, id, lat, lng, zipcode, story, diagnosis):
         self.id = id
-        self.data = data
+        self.lat = lat
+        self.lng = lng
+        self.zipcode = zipcode
+        self.story = story
+        self.diagnosis = diagnosis
+        self.datetime = (str(datetime.datetime.now())).split('.', 1)[0] #truncates to make mysql datatype valid
+
+    ##db.session.add(--python Userpin instance--)
+    ##db.session.commit() --these two in a row to add entry to database
 
 #default route to test screen
 @app.route('/')
@@ -34,17 +49,36 @@ def index():
 def getpin():
     if request.method == 'POST':
         pin = request.form.to_dict()
+        up = UserPin(pin['id'], pin['lat'], pin['lng'], pin['zipcode'], pin['story'], pin['diagnosis'])
+        db.session.add(up)
+        db.session.commit()
         addPin(pin)
     return redirect(url_for('index'))
 
 #when the addPin function appends the new pin, it writes the whole object out to a text file.
 #the text file is read back here, prepared as a results, and jsonify-ed
+#will replace with more DB ways of doing it
+
 @app.route('/givejson')
 def givejson():
     with open('data.txt', 'r') as f:
         resp = json.load(f)
         print(resp)
     return jsonify(results=resp)
+
+
+#OH FUCK YES THIS WORKS
+@app.route('/givejsondb')
+def givejsondb():
+    pins = UserPin.query.all()
+    currentPins={}
+    currentPins['pinobj'] =[]
+    for j in pins:
+        gdpn = j.__dict__
+        gdpn2 = dict([(str(k), str(v)) for k, v in gdpn.items()])
+        currentPins['pinobj'].append(gdpn2)
+    jpd=currentPins['pinobj']
+    return Response(json.dumps(jpd), mimetype='application/json')
 
 @app.route('/giveapproved')
 def giveapproved():
@@ -57,13 +91,12 @@ def giveapproved():
 
 @app.route('/approvePin/<n>')
 def approve(n):
-    approvePin(n)
+    approvePin(int(n))
     return redirect(url_for('index'))
 
 @app.route('/denyPin/<n>')
 def deny(n):
-    x = int(n)
-    denyPin(x)
+    denyPin(int(n))
     return redirect(url_for('index'))
 
 
--- userpins.py
+++ userpins.py
@@ -4,13 +4,16 @@ userPins = {}
 userPins['pinobj'] = []
 pinApproved = []
 
+userPins2 = {}
+userPins2['pinobj'] = []
+
 #writes the pins to a txt file that can be returned on a route as a json object
 def pinsToJson(n):
     with open('data.txt', 'w') as outfile:
         json.dump(n, outfile)
 
-def approvedToJson(n):
-    with open('data-approved.txt', 'w') as outfile:
+def pinsToJsonDb(n):
+    with open('data-db.txt', 'w') as outfile:
         json.dump(n, outfile)
 
 def addPin(n):
@@ -22,15 +25,17 @@ def giveApprovedPins(pinApproved):
     approvedToJson(pinApproved)
 
 #will clear out the story part of a pin before moving it on the approved object
-#this won't work but figure out how to do it
+#i think this works now
 def denyPin(n):
-    x = int(n)
-    userPins['pinobj'][x]['story'] = ''
-    f = userPins['pinobj'][x]
+    userPins['pinobj'][int(n)]['story'] = ''
+    f = userPins['pinobj'][int(n)]
     pinApproved.append(f)
 
 #approves pins with no edits
 def approvePin(n):
-    x = int(n)
-    f = userPins['pinobj'][x]
+    f = userPins['pinobj'][int(n)]
     pinApproved.append(f)
+
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+        for c in inspect(obj).mapper.column_attrs}
