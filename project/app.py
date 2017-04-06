470d9936ae5062541adfdc6d9e5aa2e3dab252e6
bearreport <bayerj@gmail.com>
2017-04-06 13:44:34 -0400

    okay

--- project/app.py
+++ project/app.py
@@ -127,7 +127,6 @@ def admin():
 
 #run flask app
 if __name__ == '__main__':
-    app.use(allowCrossDomain)
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
