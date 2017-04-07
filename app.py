afd994bfa2c0f701050e00394267402ff14ce814
Cray J Thomas Jr <cjthoma1@gmail.com>
2017-04-07 01:56:48 -0400

    Added Oakland Pittsburgh, PA to The Stigma page

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ import urllib2
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE-URL'] =''
 
-f = urllib2.urlopen('http://freegeoip.net/json/')
-json_string = f.read()
-f.close()
-location = json.loads(json_string)
-print(location)
+# f = urllib2.urlopen('http://freegeoip.net/json/')
+# json_string = f.read()
+# f.close()
+# location = json.loads(json_string)
+# print(location)
 
 #default route to test screen
 
 @app.route('/')
 def index():
-    return render_template('stigma.html')
+    return render_template('about.html')
 
 @app.route('/Home')
 def home():
-    return render_template('stigma.html')
+    return render_template('about.html')
 
 @app.route('/Story')
 def story():
